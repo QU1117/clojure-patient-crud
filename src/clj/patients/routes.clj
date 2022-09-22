52210a2ab1093688d380d72b734872ab3d66d37f
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-09-22 22:37:28 +0300

    feat: add new route for update contact action

--- src/clj/patients/routes.clj
+++ src/clj/patients/routes.clj
@@ -1,6 +1,7 @@
 (ns patients.routes
   (:require [patients.handlers :refer [get-patients
-                                       create-patient]]
+                                       create-patient
+                                       update-patient-by-id]]
             [schema.core :as schema]))
 
 (def patients-route
@@ -13,5 +14,14 @@
                                     :gender schema/Str
                                     :date_of_birth schema/Str
                                     :address schema/Str
-                                    :chi_number schema/Int}}}}]])
-
+                                    :chi_number schema/Int}}}}]
+   
+   ["/:id" {:parameters {:path {:id schema/Int}}
+            :patch {:handler update-patient-by-id
+                    :parameters {:body {:first_name schema/Str
+                                        :middle_name schema/Str
+                                        :last_name schema/Str
+                                        :gender schema/Str
+                                        :date_of_birth schema/Str
+                                        :address schema/Str
+                                        :chi_number schema/Int}}}}]])
