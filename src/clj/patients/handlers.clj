f07323ca263d1f14c591f926ead4e3363adba925
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-09-22 22:40:15 +0300

    feat & test: Green: add proper handler for patch route

--- src/clj/patients/handlers.clj
+++ src/clj/patients/handlers.clj
@@ -11,3 +11,17 @@
         created-resource (db/create-record data)]
     {:status 201
      :body created-resource}))
+
+(defn update-patient-by-id
+  [{:keys [parameters]}]
+  (let [id (get-in parameters [:path :id])
+        body (:body parameters)
+        data (assoc body :id id)
+        update-count (:next.jdbc/update-count
+                      (db/update-record-by-id data))]
+    (if (= update-count 0)
+      {:status 404
+       :body "Contact not found"}
+      
+      {:status 200
+       :body data})))
