52fe46dc82247e530022d49481560f6d36c8255a
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-04 01:53:58 +0300

    Add Hermes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/cljs/patients/*.cljs",
-	   "./src/cljs/patients/components/*.cljs"],
+	    "./src/cljs/patients/components/*.cljs",
+	    "./resources/public/*.html"],
   theme: {
     fontFamily: {
       'cinzel': ['Cinzel']
+    },
+    backgroundSize: {
+      '15%': '15%',
     },
     extend: {},
   },
