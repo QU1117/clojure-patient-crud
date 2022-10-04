fa0b53320dccdcec38eddc1f6a5da6aea0b5d6f9
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-04 21:49:37 +0300

    Add style to a patient component

--- src/cljs/patients/components/patient.cljs
+++ src/cljs/patients/components/patient.cljs
@@ -1,7 +1,9 @@
 (ns patients.components.patient)
 
 (defn patient-row [patient]
-  [:div {:class "grid grid-cols-3"}
+  [:div {:class "grid grid-cols-4 font-cinzel gap-x-4 hover:border border-black"}
+   [:div {:class "flex flex-row-reverse"}
+    (:id patient)]
    [:div (:first_name patient)]
    [:div (:middle_name patient)]
    [:div (:last_name patient)]])
