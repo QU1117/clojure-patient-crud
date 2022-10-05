e58b8d72e5c095be1ca2269e34ad10c557bde3da
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-06 01:37:39 +0300

    Add "Show all patients" in the navbar

--- src/cljs/patients/components/navigation.cljs
+++ src/cljs/patients/components/navigation.cljs
@@ -1,12 +1,20 @@
 (ns patients.components.navigation)
 
 (defn nav []
-  [:nav {:class "flex flex-initial"}
-    [:div {:class
-           "text-xl
-           m-2
-           font-cinzel
-           hover:bg-black
-           hover:text-white
-           hover:cursor-pointer"}
-     "Add new patient"]])
+  [:nav {:class "flex
+                flex-initial
+                flex-row"}
+   [:div {:class "text-xl
+                 m-2
+                 font-cinzel
+                 hover:bg-black
+                 hover:text-white
+                 hover:cursor-pointer"}
+    "Show all patients"]
+   [:div {:class "text-xl
+                 m-2
+                 font-cinzel
+                 hover:bg-black
+                 hover:text-white
+                 hover:cursor-pointer"}
+    "Add new patient"]])
