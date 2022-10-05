2296a11fb5755894832ca9e5137c0a22c33e5431
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-05 22:53:17 +0300

    Some minor styling for nav and "Add new patient" button (added flex, cursor change)

--- src/cljs/patients/components/header.cljs
+++ src/cljs/patients/components/header.cljs
@@ -4,15 +4,14 @@
   [:header
    [:h1 {:class "m-3 font-cinzel text-2xl"}
     "Patients"]
-   [:nav
+   [:nav {:class "flex flex-initial"}
     [:div {:class
            "text-xl
            m-2
            font-cinzel
            hover:bg-black
            hover:text-white
-           hover:cursor-pointer
-           w-fit"}
+           hover:cursor-pointer"}
      "Add new patient"]]
    [:hr
     {:class
