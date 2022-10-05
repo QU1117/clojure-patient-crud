5d09d5187b2a57c96ae50419f613afa6e6a92af6
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-05 20:27:20 +0300

    Add "Add new patient" button in nav

--- src/cljs/patients/components/header.cljs
+++ src/cljs/patients/components/header.cljs
@@ -4,6 +4,16 @@
   [:header
    [:h1 {:class "m-3 font-cinzel text-2xl"}
     "Patients"]
+   [:nav
+    [:div {:class
+           "text-xl
+           m-2
+           font-cinzel
+           hover:bg-black
+           hover:text-white
+           hover:cursor-pointer
+           w-fit"}
+     "Add new patient"]]
    [:hr
     {:class
      "border-0 h-px bg-gradient-to-r from-transparent via-black mb-12"}]])
