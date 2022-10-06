f64e1295096cbac04b51c9b4133415a4025cdc98
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-06 19:59:00 +0300

    Revert "Add public JS folder"
    This reverts commit e50d269321141a5c633dac13d67ae7e9d29c5809.

--- resources/public/js/cljs-runtime/patients.components.main_container.js
+++
@@ -1,7 +0,0 @@
-goog.provide('patients.components.main_container');
-patients.components.main_container.main_container_state = reagent.core.atom.cljs$core$IFn$_invoke$arity$1(null);
-patients.components.main_container.main_container = (function patients$components$main_container$main_container(){
-return new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"grid grid-cols-2 justify-items-center"], null),(((cljs.core.deref(patients.components.main_container.main_container_state) == null))?null:new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.deref(patients.components.main_container.main_container_state)], null)),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [patients.components.patient_detail.patient_detail_modal], null)], null);
-});
-
-//# sourceMappingURL=patients.components.main_container.js.map
--- resources/public/js/cljs-runtime/shadow.module.core.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('core');
-
-try { patients.core.run(); } catch (e) { console.error("An error occurred when calling (patients.core/run)"); throw(e); }
