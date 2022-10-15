dc3b2418699c09a807bc27d1c7d3ec07d0179db9
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-15 16:07:30 +0300

    Create "add-patient-form" component using fork

--- src/cljs/patients/components/add_patient_form_container.cljs
+++ src/cljs/patients/components/add_patient_form_container.cljs
@@ -1,4 +1,5 @@
-(ns patients.components.add-patient-form-container)
+(ns patients.components.add-patient-form-container
+  (:require [fork.reagent :as fork]))
 
 (defn- validation [values]
   (let [first-name    (get values "first-name" "")
@@ -30,9 +31,115 @@
       
       (empty? chi-number)
       (assoc "chi-number" "Please specify patient's CHI number")
-      
-      (not (int? (int chi-number)))
-      (assoc "chi-number" "Please specify correct CHI number")
 
       (not (= 16 (count chi-number)))
       (assoc "chi-number" "CHI number must be 16 digits"))))
+
+(defn add-patient-form []
+  [fork/form
+   {:prevent-default? true
+    :on-submit #(js/alert (:values %))
+    :validation validation}
+   (fn [{:keys [form-id
+                values
+                handle-change
+                handle-blur
+                handle-submit
+                touched
+                errors]}]
+     (let [render-error #(when (and (touched %) (get errors %))
+                           [:div (get errors %)])]
+       [:form
+        {:id form-id
+         :on-submit handle-submit}
+        [:label {:for "first-name"}
+         "First name:"]
+        [:input
+         {:type "text"
+          :name "first-name"
+          :id "first-name"
+          :value (values "first-name")
+          :on-change handle-change
+          :on-blur handle-blur}]
+        [render-error "first-name"]
+
+        [:label {:for "middle-name"}
+         "Middle name:"]
+        [:input
+         {:type "text"
+          :name "middle-name"
+          :id "middle-name"
+          :value (values "middle-name")
+          :on-change handle-change
+          :on-blur handle-blur}]
+        [render-error "middle-name"]
+
+        [:label {:for "last-name"}
+         "Last name:"]
+        [:input
+         {:type "text"
+          :name "last-name"
+          :id "last-name"
+          :value (values "last-name")
+          :on-change handle-change
+          :on-blur handle-blur}]
+        [render-error "last-name"]
+
+        [:label {:for "gender"}
+         "Gender:"]
+        [:select
+         {:id "gender"
+          :name "gender"
+          :value (values "gender")
+          :on-change handle-change}
+         [:option
+          {:selected true
+           :disabled true}
+          "Please specify patient's gender"]
+         [:option
+          {:value "Male"}
+          "Male"]
+         [:option
+          {:value "Female"}
+          "Female"]
+         [:option
+          {:value "Other"}
+          "Other"]]
+        [render-error "gender"]
+
+        [:label {:for "date-of-birth"}
+         "Date of birth:"]
+        [:input
+         {:type "date"
+          :id "date-of-birth"
+          :name "date-of-birth"
+          :value (values "date-of-birth")
+          :on-change handle-change
+          :on-blur handle-blur}]
+        [render-error "date-of-birth"]
+
+        [:label {:for "address"}
+         "Address:"]
+        [:input
+         {:type "text"
+          :name "address"
+          :id "address"
+          :value (values "address")
+          :on-change handle-change
+          :on-blur handle-blur}]
+        [render-error "address"]
+        
+        [:label {:for "chi-number"}
+         "CHI number:"]
+        [:input
+         {:type "number"
+          :name "chi-number"
+          :id "chi-number"
+          :value (values "chi-number")
+          :on-change handle-change
+          :on-blur handle-blur}]
+        [render-error "chi-number"]
+
+        [:button
+         {:type "submit"}
+         "Submit"]]))])
