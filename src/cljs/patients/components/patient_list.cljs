6030f2bb7ed4b26341c299c4af9add252db94531
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-18 17:54:07 +0300

    Refactor patient-list component into Form-2

--- src/cljs/patients/components/patient_list.cljs
+++ src/cljs/patients/components/patient_list.cljs
@@ -3,19 +3,15 @@
             [reagent.core :as r]
             [patients.components.patient-row :refer [patient-row]]))
 
-(defonce patient-list-state (r/atom nil))
-
-(defn get-patients []
-  (ajax/GET "http://localhost:4000/api/patients/"
-            {:handler (fn [resp]
-                        (reset! patient-list-state resp))}))
-
-(get-patients)
-
 (defn patient-list []
-  [:div {:class "col-start-1 col-end-2"}
-   [:ul
-    (for [patient @patient-list-state]
-      [:li {:key (:id patient)
-            :class "hover:cursor-pointer"}
-       [patient-row patient]])]])
+  (let [patient-list-state (r/atom nil)]
+    (fn []
+      (ajax/GET "http://localhost:4000/api/patients/"
+                {:handler (fn [resp]
+                            (reset! patient-list-state resp))})
+      [:div {:class "col-start-1 col-end-2"}
+       [:ul
+        (for [patient @patient-list-state]
+          [:li {:key (:id patient)
+                :class "hover:cursor-pointer"}
+           [patient-row patient]])]])))
