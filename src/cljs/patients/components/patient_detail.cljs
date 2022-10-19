511d263dad1346141394c050df9837d29327fcd2
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-19 21:48:11 +0300

    Add button in patient details modal component for updating patient's info

--- src/cljs/patients/components/patient_detail.cljs
+++ src/cljs/patients/components/patient_detail.cljs
@@ -1,6 +1,9 @@
 (ns patients.components.patient-detail
   (:require [reagent.core :as r]
-            [ajax.core :as ajax]))
+            [ajax.core :as ajax]
+            [patients.components.main-container :refer [main-container-state]]
+            [patients.components.update-patient-form :refer [update-patient-form
+                                                             update-patient-state]]))
 
 (def modal-state (r/atom "hidden"))
 (def detailed-info-state (r/atom nil))
@@ -15,8 +18,9 @@
    [:div "Middle name: " (:middle_name @detailed-info-state)]
    [:div "Last name: " (:last_name @detailed-info-state)]
    [:div "Gender: " (:gender @detailed-info-state)]
-   [:div "Date of birth: " (.format (js/Intl.DateTimeFormat "en-GB")
-                                    (:date_of_birth @detailed-info-state))]
+   [:div "Date of birth: " (->>
+                            (:date_of_birth @detailed-info-state)
+                            (.format (js/Intl.DateTimeFormat "en-GB")))]
    [:div "Address: " (:address @detailed-info-state)]
    [:div "CHI number: " (:chi_number @detailed-info-state)]
    [:br]
@@ -26,6 +30,12 @@
    [:div {:class "hover:bg-black hover:text-white hover:cursor-pointer w-fit"
           :on-click #(reset! modal-state "hidden")}
     "[Close]"]
+
+   [:div {:class "hover:bg-black hover:text-white hover:cursor-pointer w-fit"
+          :on-click #((reset! update-patient-state @detailed-info-state)
+                      (reset! main-container-state update-patient-form)
+                      (reset! modal-state "hidden"))}
+   "[Update patient's info"]
 
    [:div {:class "hover:bg-black hover:text-white hover:cursor-pointer w-fit"
          :on-click
