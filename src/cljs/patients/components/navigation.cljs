ea4ee4c118c16b66aaae6fa1ee0628abbf3160d7
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-10-27 17:09:52 +0300

    add spaces in navbar code for readability

--- src/cljs/patients/components/navigation.cljs
+++ src/cljs/patients/components/navigation.cljs
@@ -1,14 +1,17 @@
 (ns patients.components.navigation
   (:require [patients.components.main-container :refer [main-container-state]]
-            [patients.components.patients-main-container :refer [patients-main-container]]
+            [patients.components.patients-main-container
+             :refer [patients-main-container]]
             [patients.components.patient-detail :refer [modal-state]]
-            [patients.components.add-patient-form-container :refer [add-patient-form]]
+            [patients.components.add-patient-form-container
+             :refer [add-patient-form]]
             [patients.components.search-form-component :refer [search-form]]))
 
 (defn nav []
   [:nav {:class "flex
                 flex-initial
                 flex-row"}
+   
    [:div {:class "text-xl
                  m-2
                  font-cinzel
@@ -17,6 +20,7 @@
                  hover:cursor-pointer"
           :on-click #(reset! main-container-state patients-main-container)}
     "Show all patients"]
+
    [:div {:class "text-xl
                  m-2
                  font-cinzel
@@ -26,6 +30,7 @@
           :on-click #((reset! main-container-state add-patient-form)
                       (reset! modal-state "hidden"))}
     "Add new patient"]
+
    [:div {:class "text-xl
                   m-2
                   font-cinzel
