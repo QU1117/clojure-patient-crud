a8e29025b297c69db08174e73750d3c7495228f1
Dmitry Boyko <n30nv4mp@yandex.ru>
2022-11-06 15:15:36 +0300

    Add reactive filtering in patient-list

--- src/cljs/patients/components/patient_list.cljs
+++ src/cljs/patients/components/patient_list.cljs
@@ -1,7 +1,10 @@
 (ns patients.components.patient-list
   (:require [ajax.core :as ajax]
             [reagent.core :as r]
-            [patients.components.patient-row :refer [patient-row]]))
+            [patients.components.patient-row :refer [patient-row]]
+            [patients.components.filter :refer [filter-box
+                                                gender-filter-checks]]
+            [clojure.string :as cljstr]))
 
 (defn patient-list []
   (let [patient-list-state (r/atom nil)]
@@ -10,8 +13,22 @@
                 {:handler (fn [resp]
                             (reset! patient-list-state resp))})
       [:div {:class "col-start-1 col-end-2"}
-       [:ul
-        (for [patient @patient-list-state]
-          [:li {:key (:id patient)
-                :class "hover:cursor-pointer"}
-           [patient-row patient]])]])))
+       [:div {:class "flex"}
+        
+        [filter-box]
+
+        (if (empty? @gender-filter-checks)
+          [:ul
+           (for [patient @patient-list-state]
+             [:li {:key (:id patient)
+                   :class "hover:cursor-pointer"}
+              [patient-row patient]])]
+          
+          [:ul
+           (doall (for [patient @patient-list-state
+                        :when (boolean
+                               (some #{(cljstr/lower-case (:gender patient))}
+                                     @gender-filter-checks))]
+                    [:li {:key (:id patient)
+                          :class "hover:cursor-pointer"}
+                     [patient-row patient]]))])]])))
